e456dae79225add1a0290429d6f230aaaec5da67
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-10 01:18:33 -0300

    Saving model

--- src/main/java/br/com/testeitau/transacoes/model/Transacao.java
+++ src/main/java/br/com/testeitau/transacoes/model/Transacao.java
@@ -1,5 +1,30 @@
 package br.com.testeitau.transacoes.model;
 
-public class Transacao {
+import java.time.OffsetDateTime;
 
+
+public class Transacao {
+  private OffsetDateTime dataHora;
+  private double valor;
+
+  public OffsetDateTime getDataHora() {
+    return dataHora;
+  }
+  public void setDataHora(OffsetDateTime dataHora) {
+    this.dataHora = dataHora;
+  }
+  public double getValor() {
+    return valor;
+  }
+  public void setValor(double valor) {
+    this.valor = valor;
+  }
+  
+  public Transacao(OffsetDateTime dataHora, double valor) {
+    this.dataHora = dataHora;
+    this.valor = valor;
+  }
+  
+  
+  
 }
