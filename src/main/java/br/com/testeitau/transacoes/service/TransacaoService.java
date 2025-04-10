e6704a13c4431393669c7ccf85116c5c4455cfcd
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-09 22:56:42 -0300

    Adding method to return transaction statistics

--- src/main/java/br/com/testeitau/transacoes/service/TransacaoService.java
+++ src/main/java/br/com/testeitau/transacoes/service/TransacaoService.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import br.com.testeitau.transacoes.model.Transacao;
@@ -21,6 +22,16 @@ public class TransacaoService {
 
   public void clearTransactions() {
     transactionList.clear();
+  }
+
+  public Map<OffsetDateTime, List<Transacao>> getTransactionStatistics() {
+      OffsetDateTime now = OffsetDateTime.now();
+
+      return transactionList.stream()
+                            .filter(transacao -> transacao.getDataHora().isAfter(now.minusSeconds(60)))
+                            .collect(Collectors.groupingBy(
+                              transacao -> transacao.getDataHora()
+                            ));
   }
 
 }
