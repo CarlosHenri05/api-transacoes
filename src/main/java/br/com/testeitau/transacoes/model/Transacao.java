64976c6c8d2446b47ff4274d224cdf1f18065a0e
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-09 22:45:49 -0300

    first commit

---
+++ src/main/java/br/com/testeitau/transacoes/model/Transacao.java
@@ -0,0 +1,5 @@
+package br.com.testeitau.transacoes.model;
+
+public class Transacao {
+
+}
---
+++ src/test/java/br/com/testeitau/transacoes/ApItransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.testeitau.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApItransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
