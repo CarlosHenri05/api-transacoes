f618e50def8f31d6ad38dfbe91b59ba4ba726c96
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-16 17:57:21 -0300

    Adding dependencies to test with JUnit

---
+++ src/test/java/br/com/testeitau/transacoes/service/TransactionServiceTest.java
@@ -0,0 +1,17 @@
+package br.com.testeitau.transacoes.service;
+
+import br.com.testeitau.transacoes.model.Transacao;
+import br.com.testeitau.transacoes.service.TransacaoService;
+import br.com.testeitau.transacoes.exceptions.TransactionException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransacaoServiceTest {
+
+    
+}
